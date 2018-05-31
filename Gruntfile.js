ad038ae99addf4062f3abae424c140995526bcbb
Yilun <zyl.skysniper@gmail.com>
2018-05-31 17:39:33 +0800

    Use grunt for browser dist version
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    browserify: {
+      dist: {
+        files: {
+          'dist/nkn.js': [ 'lib/nkn.js' ]
+        },
+        options: {
+          browserifyOptions: {
+            standalone: 'nkn'
+          }
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'dist/nkn.min.js' : [ 'dist/nkn.js' ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+
+  grunt.registerTask('dist', ['browserify', 'uglify']);
+};
