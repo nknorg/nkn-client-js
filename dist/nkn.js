08586fe4d1f68fca212e17c17e42f4bef8b26cf9
Yilun <zyl.skysniper@gmail.com>
2020-01-14 15:22:05 -0800

    Add option to forcely use wss client
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- dist/nkn.js
+++ dist/nkn.js
@@ -292,10 +292,16 @@ function Client(key, identifier, options = {}) {
 };
 
 function isTls() {
+  if (this.options && Is.bool(this.options.tls)) {
+    return this.options.tls;
+  }
   if (typeof window === 'undefined') {
     return false;
   }
-  return window.location && window.location.protocol === "https:";
+  if (window.location && window.location.protocol === "https:") {
+    return true;
+  }
+  return false;
 }
 
 function newWsAddr(nodeInfo) {
@@ -309,7 +315,7 @@ function newWsAddr(nodeInfo) {
 
   var ws;
   try {
-    ws = new WebSocket((isTls() ? 'wss://' : 'ws://') + nodeInfo.addr);
+    ws = new WebSocket((isTls.call(this) ? 'wss://' : 'ws://') + nodeInfo.addr);
     ws.binaryType = "arraybuffer";
   } catch (e) {
     console.log('Create WebSocket failed,', e);
@@ -396,7 +402,7 @@ Client.prototype.connect = async function () {
   try {
     let res = await rpcCall(
       this.options.seedRpcServerAddr,
-      isTls() ? 'getwssaddr' : 'getwsaddr',
+      isTls.call(this) ? 'getwssaddr' : 'getwsaddr',
       { address: this.addr },
     );
     newWsAddr.call(this, res);
